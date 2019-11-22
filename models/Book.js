a66d77ea2be70aa09b96e4a57f7dcab736610a5e
Yuriy Koziy <yukoziy@gmail.com>
2019-11-22 11:46:01 -0500

    Fixed Book model

--- models/Book.js
+++ models/Book.js
@@ -5,5 +5,7 @@ let BookSchema = new Schema({
   title: {type: String, required: true},
   commentcount: {type: Number, default: 0},
   comments: [{body: String, posted_on: Date}],
-  added_on: {type: Date, default: Date.now} 
+  added_on: {type: Date, default: Date.now}
 });
+
+module.exports = mongoose.model('Book', BookSchema);
